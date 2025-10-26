ef9ff8ba133e8a42ebf504efe17f10839959aa86
CCosta007 <s00339973@myacu.edu.au>
2025-10-26 16:08:35 +1100

    About me pt2, resource page edit, dark theme
    About me 2.html:
    - The learn more button on about me.html section now links to extra information found on about me 2.html
    
    resources.html:
    - thumbnail for first resource is much bigger and visually appealing 
    
    JS:
    - I have implemented my dark theme test JavaScript from the other week and applied it to the index thus far. It seems to be working well

---
+++ assets/js/dark-mode-logic.js
@@ -0,0 +1,21 @@
+const theButton = document.getElementById("darkModeBtn");
+const body = document.body;
+
+
+if (localStorage.getItem("theme") === "dark"){ /*This will check if the theme has been clicked before from a prev visit */
+    body.classList.add("dark"); /*if it is dark, then the CSS class dark will be activated for the styling*/
+    theButton.textContent = "Enable Light mode"; /*change the button text to light mode*/
+}
+
+theButton.addEventListener('click', () => {
+    body.classList.toggle("dark"); /*uses toggle method to add, or remove the dark class with each click */
+
+    const isDark = body.classList.contains("dark"); /*checks whether dark is in the body. True if it is, false if it is not*/
+    
+    if(isDark){
+        theButton.textContent = "Enable Light Mode"; /*If isDark is true, page is currently in dark mode and the button needs to disply light mode */
+    }else{
+        theButton.textContent = "Enable Dark Mode";/*If isDark is false, page is currently in light mode and the button needs to disply dark mode */
+    }
+    localStorage.setItem("theme", isDark ? "dark" : "light"); /*will save the choice of theme in local storage*/
+});
