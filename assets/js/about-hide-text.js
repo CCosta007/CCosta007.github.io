3af3c2e0ecf4cc5f2ebfaf36205274d659d953ac
CCosta007 <s00339973@myacu.edu.au>
2025-10-27 17:41:58 +1100

    JS hide show buttons working for about and about2
    The buttons now hide and show text, and also change with the dark mode theme. The JS is incredibly messy but it works.

--- assets/js/about-hide-text.js
+++ assets/js/about-hide-text.js
@@ -2,37 +2,105 @@
 const genInfoText = document.getElementById("genInfo");
 const placementText = document.getElementById("placOv");
 const goalsText = document.getElementById("futGoals");
+const profInText = document.getElementById("profInfP");
+const placfOvText = document.getElementById("placInP");
 
 //buttons
 const infoBtn = document.getElementById("genInfoBtn");
 const placementBtn = document.getElementById("placOvBtn");
 const goalsBtn = document.getElementById("futGoalsBtn");
+const profBtn = document.getElementById("profOvBtn");
+const placBtn = document.getElementById("PlacInBtn");
 
-//start the paragraphs hidden
-genInfoText.style.display = "none";
-placementText.style.display = "none";
-goalsText.style.display = "none";
+//start the paragraphs hidden. Put as an if statement in case the buttons don't exist
+if(genInfoText && placementText && goalsText){
+    genInfoText.style.display = "none";
+    placementText.style.display = "none";
+    goalsText.style.display = "none";
+}
+if((profBtn && placBtn)){
+    profInText.style.display = "none";
+    placfOvText.style.display = "none";
+}
 
 //function to hide the text
 function hideText(p, b){
     const isDark = document.body.classList.contains("dark"); /*will be true if it is in dark mode, false if it isn't */
-    
-    if(p.style.display === "none" && isDark){
+    if(p.style.display === "none" && isDark){ //if it enters this block then it will display the text and is in dark mode, so it will change the arrow to the dark hide arrow
         p.style.display = "block"
         b.style.backgroundImage = "url(images/hide-arrow-dark.png)" 
-    }else if(p.style.display === "none" && !isDark){
+    }else if(p.style.display === "none" && !isDark){ //if it enters this block then it will display the text and is in light mode, so it will change the arrow to the light hide arrow
         p.style.display = "block"
         b.style.backgroundImage = "url(images/hide-arrow.png)" 
-    }else if(!(p.style.display === "none") && isDark){
+    }else if(!(p.style.display === "none") && isDark){ //if it enters this block then it will hide the text and is in dark mode, so it will change the arrow to the dark show arrow
         p.style.display = "none"
         b.style.backgroundImage = "url(images/show-arrow-dark.png)" 
-    }else if(!(p.style.display === "none") && !isDark){
+    }else if(!(p.style.display === "none") && !isDark){ //if it enters this block then it will hide the text and is in light mode, so it will change the arrow to the light show arrow
         p.style.display = "none"
         b.style.backgroundImage = "url(images/show-arrow.png)"  
     }
+};
+
+//function to update the buttons to dark or light mode. Exported so the dark mode logic file can utilize.
+//This function is not great and I think there would be a cleaner way to implement it, but this was the best I could come up with 
+export function updateButtonTheme(){
+    const isDark = document.body.classList.contains("dark"); /*will be true if it is in dark mode, false if it isn't */
+    if(profBtn){
+        if (profInText.style.display === "none"){
+            profBtn.style.backgroundImage = isDark ? "url(images/show-arrow-dark.png)": "url(images/show-arrow.png)"; //will set the show dark arrow if dark theme is active, else it will set light arrow
+        }else{
+            profBtn.style.backgroundImage = isDark ? "url(images/hide-arrow-dark.png)": "url(images/hide-arrow.png)"; //will set the hide dark arrow if dark theme is active, else it will set hide light arrow
+        }
+    }
+    if(placBtn){
+        if (placfOvText.style.display === "none"){
+            placBtn.style.backgroundImage = isDark ? "url(images/show-arrow-dark.png)": "url(images/show-arrow.png)"; //will set the show dark arrow if dark theme is active, else it will set light arrow
+
+        }else{
+            placBtn.style.backgroundImage = isDark ? "url(images/hide-arrow-dark.png)": "url(images/hide-arrow.png)"; //will set the hide dark arrow if dark theme is active, else it will set hide light arrow
+        }
+    }
+    else{
+        if (genInfoText.style.display === "none"){
+            infoBtn.style.backgroundImage = isDark ? "url(images/show-arrow-dark.png)": "url(images/show-arrow.png)"; //will set the show dark arrow if dark theme is active, else it will set light arrow
+        }else{
+            infoBtn.style.backgroundImage = isDark ? "url(images/hide-arrow-dark.png)": "url(images/hide-arrow.png)"; //will set the hide dark arrow if dark theme is active, else it will set hide light arrow
+        }
+
+        if (placementText.style.display === "none"){ //does same as above for placement button
+            placementBtn.style.backgroundImage = isDark ? "url(images/show-arrow-dark.png)": "url(images/show-arrow.png)";
+        }else{
+            placementBtn.style.backgroundImage = isDark ? "url(images/hide-arrow-dark.png)": "url(images/hide-arrow.png)"; 
+        }
+
+        if (goalsText.style.display === "none"){ //does same as above for goals button
+            goalsBtn.style.backgroundImage = isDark ? "url(images/show-arrow-dark.png)": "url(images/show-arrow.png)";
+        }else{
+            goalsBtn.style.backgroundImage = isDark ? "url(images/hide-arrow-dark.png)": "url(images/hide-arrow.png)"; 
+        }
+    }
+
+};
+
+//event listeners on the buttons to hide/show text. Put them in if statements in case the buttons don't exist
+if(infoBtn){
+    infoBtn.addEventListener("click", () => hideText(genInfoText, infoBtn));
+    updateButtonTheme();
 }
 
-//event listeners on the buttons to hide/show text
-infoBtn.addEventListener("click", () => hideText(genInfoText, infoBtn));
-placementBtn.addEventListener("click", () => hideText(placementText, placementBtn));
-goalsBtn.addEventListener("click", () => hideText(goalsText, goalsBtn));
+if(placementBtn){
+    placementBtn.addEventListener("click", () => hideText(placementText, placementBtn));
+    updateButtonTheme();
+}
+if(goalsBtn){
+    goalsBtn.addEventListener("click", () => hideText(goalsText, goalsBtn));
+    updateButtonTheme();
+}
+if(profBtn){
+    profBtn.addEventListener("click", () => hideText(profInText, profBtn));
+    updateButtonTheme();
+}
+if(placBtn){
+    placBtn.addEventListener("click", () => hideText(placfOvText, placBtn));
+    updateButtonTheme();
+}
--- assets/js/dark-mode-logic.js
+++ assets/js/dark-mode-logic.js
@@ -1,3 +1,5 @@
+import {updateButtonTheme} from "./about-hide-text.js";
+
 const theButton = document.getElementById("darkModeBtn");
 const body = document.body;
 
@@ -13,11 +15,15 @@ if (localStorage.getItem("theme") === "dark"){ /*This will check if the theme ha
 theButton.addEventListener('click', () => {
     body.classList.toggle("dark"); /*uses toggle method to add, or remove the dark class with each click */
     const isDark = body.classList.contains("dark"); /*checks whether dark is in the body. True if it is, false if it is not*/
-    
     if(isDark){
         theButton.textContent = "Enable Light Mode"; /*If isDark is true, page is currently in dark mode and the button needs to disply light mode */
     }else{
         theButton.textContent = "Enable Dark Mode";/*If isDark is false, page is currently in light mode and the button needs to disply dark mode */
     }
     localStorage.setItem("theme", isDark ? "dark" : "light"); /*will save the choice of theme in local storage*/
+    updateButtonTheme(); //ensures arrows are set to the correct theme
 });
+
+updateButtonTheme(); //ensures arrows are set to the correct theme
+
+
